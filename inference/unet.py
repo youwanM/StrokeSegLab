1c011c38caf3d0f51d447c6031b0d7718329e475
Yann Kerverdo <ykerverd@gpu-empenn.irisa.fr>
2025-06-23 11:42:02 +0200

    inference

---
+++ inference/unet.py
@@ -0,0 +1,161 @@
+from typing import Union, Type, List, Tuple
+
+import torch
+from dynamic_network_architectures.architectures.abstract_arch import (
+    AbstractDynamicNetworkArchitectures,
+    test_submodules_loadable,
+)
+from dynamic_network_architectures.building_blocks.helper import convert_conv_op_to_dim
+from dynamic_network_architectures.building_blocks.plain_conv_encoder import PlainConvEncoder
+from dynamic_network_architectures.building_blocks.residual import BasicBlockD, BottleneckD
+from dynamic_network_architectures.building_blocks.residual_encoders import ResidualEncoder
+from dynamic_network_architectures.building_blocks.unet_decoder import UNetDecoder
+from dynamic_network_architectures.building_blocks.unet_residual_decoder import UNetResDecoder
+from dynamic_network_architectures.initialization.weight_init import InitWeights_He
+from dynamic_network_architectures.initialization.weight_init import init_last_bn_before_add_to_0
+from torch import nn
+from torch.nn.modules.conv import _ConvNd
+from torch.nn.modules.dropout import _DropoutNd
+import nibabel as nib
+
+
+class ResidualEncoderUNet(AbstractDynamicNetworkArchitectures):
+    def __init__(
+        self,
+        input_channels: int,
+        n_stages: int,
+        features_per_stage: Union[int, List[int], Tuple[int, ...]],
+        conv_op: Type[_ConvNd],
+        kernel_sizes: Union[int, List[int], Tuple[int, ...]],
+        strides: Union[int, List[int], Tuple[int, ...]],
+        n_blocks_per_stage: Union[int, List[int], Tuple[int, ...]],
+        num_classes: int,
+        n_conv_per_stage_decoder: Union[int, Tuple[int, ...], List[int]],
+        conv_bias: bool = False,
+        norm_op: Union[None, Type[nn.Module]] = None,
+        norm_op_kwargs: dict = None,
+        dropout_op: Union[None, Type[_DropoutNd]] = None,
+        dropout_op_kwargs: dict = None,
+        nonlin: Union[None, Type[torch.nn.Module]] = None,
+        nonlin_kwargs: dict = None,
+        deep_supervision: bool = False,
+        block: Union[Type[BasicBlockD], Type[BottleneckD]] = BasicBlockD,
+        bottleneck_channels: Union[int, List[int], Tuple[int, ...]] = None,
+        stem_channels: int = None,
+    ):
+        super().__init__()
+
+        self.key_to_encoder = "encoder.stages"
+        self.key_to_stem = "encoder.stem"
+        self.keys_to_in_proj = ("encoder.stem.convs.0.conv", "encoder.stem.convs.0.all_modules.0")
+
+        if isinstance(n_blocks_per_stage, int):
+            n_blocks_per_stage = [n_blocks_per_stage] * n_stages
+        if isinstance(n_conv_per_stage_decoder, int):
+            n_conv_per_stage_decoder = [n_conv_per_stage_decoder] * (n_stages - 1)
+        assert len(n_blocks_per_stage) == n_stages, (
+            "n_blocks_per_stage must have as many entries as we have "
+            f"resolution stages. here: {n_stages}. "
+            f"n_blocks_per_stage: {n_blocks_per_stage}"
+        )
+        assert len(n_conv_per_stage_decoder) == (n_stages - 1), (
+            "n_conv_per_stage_decoder must have one less entries "
+            f"as we have resolution stages. here: {n_stages} "
+            f"stages, so it should have {n_stages - 1} entries. "
+            f"n_conv_per_stage_decoder: {n_conv_per_stage_decoder}"
+        )
+        self.encoder = ResidualEncoder(
+            input_channels,
+            n_stages,
+            features_per_stage,
+            conv_op,
+            kernel_sizes,
+            strides,
+            n_blocks_per_stage,
+            conv_bias,
+            norm_op,
+            norm_op_kwargs,
+            dropout_op,
+            dropout_op_kwargs,
+            nonlin,
+            nonlin_kwargs,
+            block,
+            bottleneck_channels,
+            return_skips=True,
+            disable_default_stem=False,
+            stem_channels=stem_channels,
+        )
+        self.decoder = UNetDecoder(self.encoder, num_classes, n_conv_per_stage_decoder, deep_supervision)
+
+    def forward(self, x):
+        skips = self.encoder(x)
+        return self.decoder(skips)
+
+    def compute_conv_feature_map_size(self, input_size):
+        assert len(input_size) == convert_conv_op_to_dim(self.encoder.conv_op), (
+            "just give the image size without color/feature channels or "
+            "batch channel. Do not give input_size=(b, c, x, y(, z)). "
+            "Give input_size=(x, y(, z))!"
+        )
+        return self.encoder.compute_conv_feature_map_size(input_size) + self.decoder.compute_conv_feature_map_size(
+            input_size
+        )
+
+    @staticmethod
+    def initialize(module):
+        InitWeights_He(1e-2)(module)
+        init_last_bn_before_add_to_0(module)
+
+
+
+ 
+network = ResidualEncoderUNet(
+    input_channels=1,
+    n_stages=6,
+    features_per_stage=[32, 64, 128, 256, 320, 320],
+    conv_op=torch.nn.Conv3d,
+    kernel_sizes=[[3, 3, 3] for _ in range(6)],
+    strides=[[1, 1, 1], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2]],
+    n_blocks_per_stage=[1, 3, 4, 6, 6, 6],
+    num_classes=2,
+    n_conv_per_stage_decoder=[1, 1, 1, 1, 1],
+    conv_bias=True,
+    norm_op=torch.nn.InstanceNorm3d,
+    norm_op_kwargs={"eps": 1e-5, "affine": True},
+    nonlin=torch.nn.LeakyReLU,
+    nonlin_kwargs={"inplace": True},
+    deep_supervision=False,
+)
+    
+    
+def test_convert_to_fp16():
+    network.initialize(network)
+    test_submodules_loadable(network)
+    checkpoint = torch.load("model.pth", map_location="cpu", weights_only=False)
+    network.load_state_dict(checkpoint["network_weights"])
+    network = network.half()
+    for param in network.parameters():
+        param.data = param.data.half()
+    new_checkpoint = {"network_weights": network.state_dict()}
+    torch.save(new_checkpoint, "model_fp16.pth")
+    print("Checkpoint loaded successfully.")
+    
+def test_load():
+    network.initialize(network)
+    test_submodules_loadable(network)
+    checkpoint = torch.load("./model.pth", map_location="cpu", weights_only=False)
+    network.load_state_dict(checkpoint["network_weights"])
+    print("Checkpoint loaded successfully.")
+
+def test_run():
+    network.eval()
+    img = nib.load("./sub-r001s001_0000.nii.gz")
+    img=img.get_fdata().astype("float32")
+    tensor=torch.from_numpy(img).unsqueeze(0).unsqueeze(0)
+    print(tensor.size())
+    with torch.no_grad():
+        output = network(tensor)
+    print(output)
+    
+test_load()
+test_run()
