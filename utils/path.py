d67cb99d3686eb86f902e44fea190d3508293611
Youwan <youwan.mahe@orange.fr>
2025-09-01 11:06:49 +0200

    Update path.py
    Ajout du paths ProgramData pour models et atlas

--- utils/path.py
+++ utils/path.py
@@ -7,6 +7,7 @@ BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 if sys.platform == "win32":
     ROAMING = os.getenv("APPDATA")
     LOCAL = os.getenv("LOCALAPPDATA")
+    PROGRAMDATA = os.getenv("PROGRAMDATA")
 elif sys.platform == "darwin":
     # macOS
     ROAMING = os.path.expanduser("~/Library/Application Support")
@@ -18,8 +19,8 @@ else:
     
 CONFIG_FILE = os.path.join(ROAMING, APP_NAME, "config.ini")
 ANIMA_DIR = os.path.join(BASE_DIR, "anima")
-ATLAS_DIR = os.path.join(BASE_DIR, "atlas")
-MODEL_DIR = os.path.join(LOCAL,APP_NAME, "models")
+ATLAS_DIR = os.path.join(PROGRAMDATA, APP_NAME, "Atlas")
+MODEL_DIR = os.path.join(PROGRAMDATA, APP_NAME, "Models")
 LOGO_INRIA = os.path.join(BASE_DIR,"StrokeSegCore", "assets", "INRIA.png")
 LOGO = os.path.join(BASE_DIR,"StrokeSegCore", "assets", "brain.png")
 LOG_DIR = os.path.join(LOCAL,APP_NAME,"logs")
